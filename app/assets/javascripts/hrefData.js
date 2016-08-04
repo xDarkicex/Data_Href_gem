6233839522c33278310fd2cc6a590ae370ee94ee
Gentry Rolofson <xdarkicex@gmail.com>
2016-08-03 23:48:55 -0700

    Bull Shit Commit

--- app/assets/javascripts/hrefData.js
+++ app/assets/javascripts/hrefData.js
@@ -1,1 +1,8 @@
 //= require hrefData
+$(function(){
+  $('body').click(function(e){
+    var obj = $(e.target.parentNode);
+    if(obj.data('href')) window.document.location = obj.data('href');
+    if(obj.data('href-new')) window.open(obj.data('href-new'));
+  });
+});
